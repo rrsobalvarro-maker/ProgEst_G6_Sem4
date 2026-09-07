24648dca8004e21698472d01fb3e9cb5d5528057
rrsobalvarro-maker <rrsobalvarro@uamv.edu.ni>
2026-09-07 13:39:25 -0600

    Agregue proyecto calculadora

---
+++ calculadora/principal.py
@@ -0,0 +1,6 @@
+import aritmetica as arit
+
+print(f"La suma es {arit.add(2,2)}")
+print(f"La suma es {arit.add(2,2)}")
+print(f"La multiplicacion es {arit.mult(5,6)}")
+print(f"La division es {arit.div(134,2)}")
